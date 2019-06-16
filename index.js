1a401b82644d9fb0504033c6aa2d37606988e249
Crystal Ophaso <crystalo@procore.com>
2019-06-15 23:54:05 -0700

    checked state

--- index.js
+++ index.js
@@ -16,19 +16,19 @@ function generateQuiz() {
     <h2>${STORE[questionNumber].question}</h2>
     <fieldset>
     <label class="answer choice-${STORE[questionNumber].answers[0]}">
-    <input type="radio" value="${STORE[questionNumber].answers[0]}" name="answer" required>
+    <input type="radio" value="${STORE[questionNumber].answers[0]}" name="answer" checked>
     <span>${STORE[questionNumber].answers[0]}</span>
     </label>
     <label class="answer choice-${STORE[questionNumber].answers[1]}">
-    <input type="radio" value="${STORE[questionNumber].answers[1]}" name="answer" required>
+    <input type="radio" value="${STORE[questionNumber].answers[1]}" name="answer">
     <span>${STORE[questionNumber].answers[1]}</span>
     </label>
     <label class="answer choice-${STORE[questionNumber].answers[2]}">
-    <input type="radio" value="${STORE[questionNumber].answers[2]}" name="answer" required>
+    <input type="radio" value="${STORE[questionNumber].answers[2]}" name="answer">
     <span>${STORE[questionNumber].answers[2]}</span>
     </label>
     <label class="answer choice-${STORE[questionNumber].answers[3]}">
-    <input type="radio" value="${STORE[questionNumber].answers[3]}" name="answer" required>
+    <input type="radio" value="${STORE[questionNumber].answers[3]}" name="answer">
     <span>${STORE[questionNumber].answers[3]}</span>
     </label>
     <button type="submit" class="submitButton">Submit</button>
